127fc668ded29b363acc99f31c250f145e7471fe
John Oblenda <joandong2@gmail.com>
2020-02-22 13:50:46 -0500

    carousel done, also w/ working buttons

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,54 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+let images = ['mountains', 'computer', 'trees', 'turntable'];
+
+function Carousel() {
+    const carouselWrap = document.createElement('div');
+    carouselWrap.classList.add('carousel');
+    
+    const leftButton = document.createElement('div');
+    leftButton.classList.add('left-button');
+    leftButton.textContent = '<';
+    carouselWrap.appendChild(leftButton);
+
+    images.forEach((image) => {
+        let img = document.createElement('img');
+        img.setAttribute('src', `./assets/carousel/${image}.jpeg`);
+        carouselWrap.appendChild(img);
+    });
+    
+    const rightButton = document.createElement('div');
+    rightButton.classList.add('right-button');
+    rightButton.textContent = '>';
+    carouselWrap.appendChild(rightButton);
+
+    return carouselWrap;
+}
+
+const carouselContainer = document.querySelector('.carousel-container');
+carouselContainer.appendChild(Carousel());
+const leftButton = document.querySelector('.left-button');
+const rightButton = document.querySelector('.right-button');
+
+window.addEventListener('load', () => {
+    const images = Array.prototype.slice.call(document.querySelectorAll('.carousel img'));
+    let count = 0,
+        max = images.length-1;
+
+    images[count].style.display = 'block';
+    
+    leftButton.addEventListener('click', () => {
+        images[count].style.display = 'none';
+        count = (count<=0) ? max : count-=1;
+        images[count].style.display = 'block';
+    });
+
+    rightButton.addEventListener('click', () => {
+        images[count].style.display = 'none';
+        count = (count<max) ? count+=1 : 0;
+        images[count].style.display = 'block';
+    });
+
+});
